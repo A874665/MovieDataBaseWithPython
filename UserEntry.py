c20134da24c12878b7e8a7345a1151630f315bfd
Akumry <61588577+A874665@users.noreply.github.com>
2021-11-30 11:30:37 +0530

    Add files via upload

--- UserEntry.py
+++ UserEntry.py
@@ -1,15 +1,21 @@
 from CreateConnections import Connections
 mydb = Connections()
 mycursor= mydb.cursor()
-Moviename = input("Enter Name of movie: ")
-MovieActor = input("Enter Name of Movie Actor: ")
-MovieActress = input("Enter Name of Movie Actress: ")
-MovieDirector = input("Enter Name of Movie Director: ")
-MovieReleased = input("Enter Year of MovieReleased: ")
 
-sqlform="Insert into movie(name,actor,actress,director,year_of_release) values(%s,%s,%s,%s,%s)"
-values =  [(Moviename,MovieActor,MovieActress,MovieDirector,MovieReleased)]
+def UserInput():
 
-mycursor.executemany(sqlform, values)
+    Moviename = input("Enter Name of movie: ")
+    MovieActor = input("Enter Name of Movie Actor: ")
+    MovieActress = input("Enter Name of Movie Actress: ")
+    MovieDirector = input("Enter Name of Movie Director: ")
+    MovieReleased = input("Enter Year of MovieReleased: ")
 
-mydb.commit() 
+    sqlform="Insert into movie(name,actor,actress,director,year_of_release) values(%s,%s,%s,%s,%s)"
+    values =  [(Moviename,MovieActor,MovieActress,MovieDirector,MovieReleased)]
+
+    mycursor.executemany(sqlform, values)
+
+    mydb.commit() 
+
+if __name__ == "__main__":
+    UserInput()
